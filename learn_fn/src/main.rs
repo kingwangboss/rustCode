1e9b67f6fa91bd6971c225c02be2c31035235939
GuyWong <kingwangboss@gmail.com>
2024-02-19 22:37:50 +0800

    function

---
+++ learn_fn/src/main.rs
@@ -0,0 +1,30 @@
+
+fn other_fun() {
+    println!("This is a function");
+}
+
+fn other_fun1(a: i32, b: i32) {
+    println!("a = {}, b = {}", a, b);
+}
+
+fn other_fun2(a: i32, b: i32) -> i32 {
+    let result = a + b;
+    return result;
+}
+
+fn other_fun3(a: i32, b: i32) -> i32 {
+    let result = a + b;
+    result
+}
+
+fn other_fun4(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+fn main() {
+    other_fun();
+    other_fun1(1, 2);
+
+    let result = other_fun2(2, 4);
+    println!("result = {}", result);
+}
